d775d55f31591661373d3402153acd7bc817c2a8
Michał Jarek <michaljarekk@gmail.com>
2025-07-07 00:22:51 +0200

    postgresql

--- backend/transactionManager/transactionManager/settings.py
+++ backend/transactionManager/transactionManager/settings.py
@@ -86,10 +86,18 @@ WSGI_APPLICATION = "transactionManager.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
+    },
 }
 
 
