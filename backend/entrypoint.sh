ccf0362625441b50636b7e947ea2b03faa22cfee
Michał Jarek <michaljarekk@gmail.com>
2025-08-18 21:35:19 +0200

    ✏️ Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e 
 
-python manage.py migrate
+python manage.py migrate --no-input
 
-python manage.py collectstatic
+python manage.py collectstatic --no-input --clear
 
 uvicorn transactionManager.asgi:application --reload --host 0.0.0.0 --port 8000
